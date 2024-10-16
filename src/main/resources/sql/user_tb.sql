7167ad61342ebd6e42294023d169d88400920ca1
p3chaeyeon <p3chaeyeon@gmail.com>
2024-10-16 12:13:06 +0900

    uemail1, uemail2 통합 >> uemail

--- src/main/resources/sql/user_tb.sql
+++ src/main/resources/sql/user_tb.sql
@@ -4,6 +4,5 @@ CREATE TABLE user_tb (
     uname VARCHAR(50) NOT NULL,				-- 사용자 이름
     uid VARCHAR(50) NOT NULL UNIQUE,		-- 사용자 아이디
     upwd VARCHAR(100) NOT NULL,				-- 사용자 비밀번호
-    uemail1 VARCHAR(50) NOT NULL,			-- 사용자 이메일 아이디
-    uemail2 VARCHAR(50) NOT NULL				-- 사용자 이메일 도메인
+    uemail VARCHAR(100) NOT NULL            -- 사용자 이메일
 );
