410e328bd2d9dafa054b7f61d32ce7f1313a9886
p3chaeyeon <p3chaeyeon@gmail.com>
2024-10-17 17:39:54 +0900

    fdishcode ON DELETE CASCADE 제약조건 추가

--- src/main/resources/sql/favorite_tb.sql
+++ src/main/resources/sql/favorite_tb.sql
@@ -5,7 +5,7 @@ CREATE TABLE favorite_tb (
     fdishcode INT,							-- 음식 코드 (dish_tb 외래키)
     UNIQUE (fuserid, fdishcode),			-- 사용자와 음식의 중복 방지
     FOREIGN KEY (fuserid) REFERENCES user_tb(uid),	
-    FOREIGN KEY (fdishcode) REFERENCES dish_tb(dcode)	
+    FOREIGN KEY (fdishcode) REFERENCES dish_tb(dcode) ON DELETE CASCADE
 );
 
 -- 외래키
