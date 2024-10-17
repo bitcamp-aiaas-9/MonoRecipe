71f0c289cb7f6e20eff977757754b09d9b9679ff
p3chaeyeon <p3chaeyeon@gmail.com>
2024-10-17 19:34:05 +0900

    충돌 해결, import 정리

--- src/main/java/com/controller/MonoRecipe/UserController.java
+++ src/main/java/com/controller/MonoRecipe/UserController.java
@@ -3,7 +3,6 @@ package com.controller.MonoRecipe;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 
 import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpSession;
