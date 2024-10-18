2d4e8b590589f02f8e24912c8428fbe032314f40
p3chaeyeon <p3chaeyeon@gmail.com>
2024-10-18 21:18:37 +0900

    sql 쿼리 최종 정리

--- src/main/resources/sql/dish_tb.sql
+++ src/main/resources/sql/dish_tb.sql
@@ -1,22 +1,14 @@
 -- MySQL
-CREATE TABLE dish_tb (
-    dcode INT AUTO_INCREMENT PRIMARY KEY,	-- 음식 코드
-    dimage VARCHAR(255) NOT NULL,			-- 음식 사진 파일명
-    dimageUUID VARCHAR(255) NOT NULL,		-- 음식 사진 UUID
-    dname VARCHAR(100) NOT NULL,			-- 음식 이름
-    dintro TEXT,							-- 음식 소개글
-    drecipe TEXT NOT NULL,					-- 음식 레시피
-    dscore DECIMAL(5, 2) DEFAULT 0.00		-- 음식 평균 평점
-);
+-- 요리 테이블
 
 CREATE TABLE `dish_tb` (
   `dcode` int NOT NULL AUTO_INCREMENT,	-- 음식 코드
   `dimage` varchar(255) NOT NULL,		-- 음식 사진 파일명
   `dimageUUID` varchar(255) NOT NULL,	-- 음식 사진 UUID
   `dname` varchar(100) NOT NULL,		-- 음식 이름
-  `dintro` text,
-  `drecipe` text NOT NULL,
-  `dscore` decimal(5,2) DEFAULT '0.00'
+  `dintro` text,						-- 음식 소개글
+  `drecipe` text NOT NULL,				-- 음식 레시피
+  `dscore` decimal(5,2) DEFAULT '0.00'	-- 음식 평균 평점
   PRIMARY KEY (`dcode`)
 )
 
@@ -34,7 +26,7 @@ BEGIN
        WHERE rdishcode = NEW.rdishcode
    )
    WHERE dcode = NEW.rdishcode;
-END;
+END
   
    
    
@@ -50,7 +42,7 @@ BEGIN
        WHERE rdishcode = NEW.rdishcode
    )
    WHERE dcode = NEW.rdishcode;
-END;
+END
 
    
 -- 트리거: 리뷰 삭제 시 음식의 평균 평점 업데이트 
@@ -65,5 +57,4 @@ BEGIN
        WHERE rdishcode = OLD.rdishcode
    )
    WHERE dcode = OLD.rdishcode;
-END;
-
+END
